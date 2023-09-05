98d5b6e1255ed4b7e22cb3faf84eefa62459bc4d
patteg21 <patteg21@gmail.com>
2023-09-04 22:53:46 -0400

    correct react

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import { StyledEngineProvider } from '@mui/material/styles';
-import Demo from './Demo';
-
-// const elementToInsert = <div>Hello, World!</div>;
-// elementToInsert.classList("project")
-
-// ReactDOM.render(elementToInsert,document.getElementById('timeline'));
-
 const tabs = document.querySelectorAll('.ref-item a');
 const tabContent = document.querySelectorAll('.tab-page');
 
---
+++ react.js
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { StyledEngineProvider } from '@mui/material/styles';
+import Demo from './Demo';
+
+// const elementToInsert = <div>Hello, World!</div>;
+// elementToInsert.classList("project")
+
+// ReactDOM.render(elementToInsert,document.getElementById('timeline'));
